29d4831393366a91a1d42501dfb2670ba4f9d19f
laboratorioesan <marti1125@gmail.com>
2018-06-16 19:26:25 -0500

    se agrega un ejemplo bubble sort

---
+++ BUBBLE_SORT/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+// http://www.cplusplus.com/reference/algorithm/swap/
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [ESAN] Desarrollo de Aplicaciones 1
 
 [Algoritmos de Busqueda](https://visualgo.net/es/sorting)
+
+[Bubble Sort](https://www.geeksforgeeks.org/bubble-sort/)
